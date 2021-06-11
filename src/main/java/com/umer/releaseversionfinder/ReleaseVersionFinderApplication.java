7f76b1c1c490129339a72b227d0f4e63cba2abf7
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-12 00:09:51 +0200

    release-version-finder: Initial commit.

---
+++ src/main/java/com/umer/releaseversionfinder/ReleaseVersionFinderApplication.java
@@ -0,0 +1,13 @@
+package com.umer.releaseversionfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseVersionFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseVersionFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umer/releaseversionfinder/ReleaseVersionFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umer.releaseversionfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseVersionFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
